196fd77eaaab5e571cf367f5b9106707078462df
Velerium <bondaniandrea@libero.it>
2021-09-09 17:30:57 +0200

    A very, VERY basic SCSS approach.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movies!</title>
-</head>
-<body>
-    @foreach ($movieList as $movie)
-    {{ $movie->title }}<br>
-    @endforeach
-</body>
-</html>
+@extends ('templates.common')
+
+@section('content')
+    <div class="container">
+        @foreach ($movieList as $movie)
+        <div class="item">
+            <div class="itemContent"
+                style="background-image: url({{ $movie->cover }});
+                        background-size: cover;
+                        background-position: center;">
+
+                <div class="layer"></div>
+                <div class="itemInfo">
+                    <div class="titles">
+                        <h3>{{ $movie->title }}</h3>
+                        <h5>{{ $movie->original_title }}</h5>
+                    </div>
+                    <h4 class="date">{{ $movie->date }}</h4>
+                    <h4>Vote: {{ $movie->vote }}/10</h4>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+@endsection
---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,4 @@
+<div class="header">
+    <h1>Movie DB!</h1>
+    <h3>Yes I know, you've never seen such a beautiful site before.</h3>
+</div>
